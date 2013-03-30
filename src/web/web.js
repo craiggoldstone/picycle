9a6a284090d45d0954b2f5c95cab2662a01bf2cc
craigsnyders <craigsnyders@me.com>
2013-03-30 00:01:53 +0000

    minor change to message body

--- src/web/web.js
+++ src/web/web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+  response.send('second packed in response');
 });
 
 var port = process.env.PORT || 5000;
