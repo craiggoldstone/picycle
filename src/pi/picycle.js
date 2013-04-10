8a54a4fa5b7437f003707590b1176127f771e4b4
craigsnyders <craigsnyders@me.com>
2013-04-10 19:29:23 +0000

    custom log levels in winston

--- src/pi/picycle.js
+++ src/pi/picycle.js
@@ -13,27 +13,80 @@ var DEBUG_LOG_MAXSIZE = 1024 * 1024 * 10 // 10MB
 
 
 // set up the debug log
-winston.loggers.add(DEBUG_LOG_NAME);
-var logger = winston.loggers.get(DEBUG_LOG_NAME);
 fs.mkdir(LOG_DIR, function(err) {
     if (err && err.code == 'EEXIST') {
         // wait for the FILE to be set up before logging this warn
         setTimeout(function() {
             logger.warn('**100ms ago** Directory already exists: ' + LOG_DIR);
         }, 100);
-    } else {
+    } else if (err) {
         throw err;
     }
 })
-logger.remove(winston.transports.Console);
-logger.add(winston.transports.Console, {
-    level: 'notice',
-    colorize: 'true'
-});
-logger.add(winston.transports.File, {
-    filename: DEBUG_LOG_PATH,
-    maxsize: DEBUG_LOG_MAXSIZE
-});
+var customLevels = {
+    levels: {
+      debug: 0,
+      info: 1,
+      warning: 2,
+      error: 3
+    },
+    colors: {
+      debug: 'blue',
+      info: 'green',
+      warning: 'orange',
+      error: 'red'
+    }
+  };
+
+// ========================
+// create the logger
+var logger = module.exports = new(winston.Logger)({
+    level: 'debug',
+    levels: customLevels.levels,
+    //colors: customLevels.colors,
+    transports: [
+    // setup console logging
+    new(winston.transports.Console)({
+        level: 'info',
+        levels: customLevels.levels,
+        colorize: true
+    }),
+    // setup logging to mongodb
+    new(winston.transports.File)({
+        filename: './logs/picycle-debug-new.log',
+        maxsize: DEBUG_LOG_MAXSIZE,
+        level: 'debug',
+        levels: customLevels.levels
+    })]
+})
+// set the coloring
+winston.addColors(customLevels.colors)
+
+// ========================
+
+// var logger = winston.loggers.get(DEBUG_LOG_NAME);
+// fs.mkdir(LOG_DIR, function(err) {
+//     if (err && err.code == 'EEXIST') {
+//         // wait for the FILE to be set up before logging this warn
+//         setTimeout(function() {
+//             logger.warn('**100ms ago** Directory already exists: ' + LOG_DIR);
+//         }, 100);
+//     } else if (err) {
+//         throw err;
+//     }
+// })
+// logger.remove(winston.transports.Console);
+
+// winston.addColors(customLevels.colors);
+// logger.add(winston.transports.Console, {
+//     levels: customLevels,
+//     level: 'info',
+//     colorize: 'true'
+// });
+// logger.add(winston.transports.File, {
+//     filename: DEBUG_LOG_PATH,
+//     maxsize: DEBUG_LOG_MAXSIZE
+// });
 
 // set up the data log
 winston.loggers.add(DATA_LOG_NAME);
@@ -43,6 +96,18 @@ datalogger.add(winston.transports.File, {
     filename: DATA_LOG_PATH,
     maxsize: DATA_LOG_MAXSIZE
 });
+
+// var customLevelLogger = new (winston.Logger)({ levels: customLevels.levels });
+// console.log(customLevelLogger);
+// logger.log('debug', 'debug');
+// logger.log('info', 'info');
+// logger.log('warn', 'warn');
+// logger.log('error', 'error');
+// logger.log('fatal', 'fatal');
+logger.debug('DEBUG');
+logger.info('INFO');
+logger.warning('WARN');
+logger.error('ERROR');
 
 var picycle = {
     'gps': new gps(),
@@ -59,6 +124,15 @@ picycle.distance.start();
 picycle.distance.on('tick', function (data) {
     logger.debug('got new distance: ' + data.distance + ' time: ' + data.time);
     datalogger.info(data);
+});
+
+process.on('exit', function() {
+  logger.info('picycle about to exit. Uptime: ' + process.uptime() + ' seconds.');
+});
+
+process.on( 'SIGINT', function() {
+  logger.info("gracefully shutting down from  SIGINT (Ctrl-C)");
+  process.exit();
 });
 
 exports.picycle = picycle;
