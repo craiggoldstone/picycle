ad46058fbeb1af7c45084930f87123b7a86745db
craigsnyders <craigsnyders@me.com>
2013-04-11 13:12:04 +0000

    rename picycle.js to app.js, add onetime log that GPS & Distance sensors start receiving data

--- src/pi/app.js
+++ src/pi/app.js
@@ -7,11 +7,6 @@ var datalogger = logging.Logging().get(log_config.DATA_LOG_NAME);
 var gps = require('./gps/gps');
 var distance = require('./distance/distance-socket').distance;
 
-logger.debug('debug test');
-logger.info('info test');
-logger.warn('warning test');
-logger.error('error test');
-
 var picycle = {
     'gps': new gps(),
     'distance': new distance()
@@ -22,6 +17,10 @@ picycle.gps.on('location', function (location) {
     datalogger.info(location);
 });
 
+picycle.gps.once('location', function (location) {
+    logger.info('receiving data from GPS stream... OK!');
+});
+
 picycle.distance.start();
 
 picycle.distance.on('tick', function (data) {
@@ -29,8 +28,12 @@ picycle.distance.on('tick', function (data) {
     datalogger.info(data);
 });
 
+picycle.distance.once('tick', function (data) {
+    logger.info('receiving data from distance sensor... OK!');
+});
+
 process.on('exit', function() {
-  logger.info('picycle about to exit. Uptime: ' + process.uptime() + ' seconds.');
+  logger.info('picycle about to exit. Uptime: ' + Math.round(process.uptime()) + ' seconds.');
 });
 
 process.on( 'SIGINT', function() {
